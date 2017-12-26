0118fff25f0b3242c94f342601a9f22e912ac545
nguyenxuanphuong1211 <nguyenxuanphuong1211@gmail.com>
2017-12-26 11:34:30 +0700

    update_view

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,8 +1,11 @@
-<html><head>
-<title>404 Not Found</title>
-</head><body>
-<h1 style="color: red;">404 <br>Not Found</h1>
-<h2>Sorry !<br>
-The system can not find the request .</h2>
-<h3>Back <a href="{{ url('/')}}">Home</a></h3>
-</body></html>
+@extends('page.master')
+@section('content')
+<body>
+    <div class="container" style="text-align: center;">
+        <h1 style="color: red;">404 <br>Not Found</h1>
+        <h2>Sorry !<br>
+        The system can not find the request .</h2>
+        <h3>Back <a href="{{ url('/')}}">Home</a></h3>
+    </div>
+</body>
+@stop
